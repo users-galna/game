63c820e865830f80d335110606bc7c8a2f4682d6
root <root@localhost.localdomain>
2019-11-22 22:28:22 +0000

    add scripts

---
+++ katan/scripts/ControlePause.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlePause : MonoBehaviour
+{
+    public GameObject menu;
+    static public bool estapausado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        estapausado = false;
+        menu = Instantiate(menu, menu.transform.position, menu.transform.rotation) as GameObject;
+        Pause(false);
+    }
+
+    void Pause(bool statuspause)
+    {
+        estapausado = statuspause;
+        menu.SetActive(estapausado);
+        if(estapausado == true)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause(!estapausado);
+
+        }
+    }
+}
---
+++ katan/scripts/controle_menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controle_menu : MonoBehaviour
+{
+    public void modo_historia()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("nome da scene");
+    }
+    public void versus()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("nome da scene");
+    }
+    public void configuração()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("nome da scene");
+    }
+    public void sair()
+    {
+        Application.Quit();
+
+    }
+
+
+}
